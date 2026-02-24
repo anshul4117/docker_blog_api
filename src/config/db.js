ea2f9fd347d4020fe575266c315842ec76a94d85
Anshul <anshul41171@gmail.com>
2026-02-24 22:39:58 +0530

    fix: message of connected docker

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+        console.log(`✅ MongoDB Connected Successfully `);
     } catch (error) {
         console.error(`❌ MongoDB Connection Error: ${error.message}`);
         process.exit(1);
